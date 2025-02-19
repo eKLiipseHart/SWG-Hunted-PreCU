94aa9c3df4745d4e8655bbfd4fce43e3e5859152
eKLiipseHart <99631997+eKLiipseHart@users.noreply.github.com>
2025-02-18 21:27:39 -0700

    Update krayt_dragon_tissue_common.lua

--- MMOCoreORB/bin/scripts/loot/items/creature/krayt_dragon_tissue_common.lua
+++ MMOCoreORB/bin/scripts/loot/items/creature/krayt_dragon_tissue_common.lua
@@ -2,7 +2,7 @@
 
 krayt_dragon_tissue_common = {
 	minimumLevel = 0,
-	maximumLevel = 80,
+	maximumLevel = -1,
 	customObjectName = "",
 	directObjectTemplate = "object/tangible/component/weapon/blaster_power_handler_enhancement_krayt.iff",
 	craftingValues = {
